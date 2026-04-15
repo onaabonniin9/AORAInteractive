2d781bb79f2ba6e953fc2a01b615682253f553d3
agarciaalvarez8-gif <agarciaalvarez8@uoc.edu>
2026-04-15 22:16:01 +0200

    Sistema de gaze shoot funciinando
    Sistema de gaze shoot funciinando

---
+++ Assets/Scripts/Gazeshoot.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+
+public class GazeShoot : MonoBehaviour
+{
+    public float gazeTimeRequired = 1.0f;
+    public GameObject enemyPrefab;
+
+    private float gazeTimer = 0f;
+    private GameObject currentTarget = null;
+
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(
+            new Vector3(Screen.width / 2, Screen.height / 2, 0)
+        );
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.CompareTag("Enemy"))
+            {
+                if (currentTarget == hit.collider.gameObject)
+                {
+                    gazeTimer += Time.deltaTime;
+
+                    if (gazeTimer >= gazeTimeRequired)
+                    {
+                        Vector3 position = hit.collider.transform.position;
+
+                        Destroy(currentTarget);
+                        ResetGaze();
+
+                        Invoke(nameof(SpawnEnemy), 1.5f);
+                    }
+                }
+                else
+                {
+                    currentTarget = hit.collider.gameObject;
+                    gazeTimer = 0f;
+                }
+            }
+            else
+            {
+                ResetGaze();
+            }
+        }
+        else
+        {
+            ResetGaze();
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        Vector3 newPosition = new Vector3(
+            Random.Range(-5f, 5f),
+            1f,
+            Random.Range(2f, 6f)
+        );
+
+        Instantiate(enemyPrefab, newPosition, Quaternion.identity);
+    }
+
+    void ResetGaze()
+    {
+        gazeTimer = 0f;
+        currentTarget = null;
+    }
+}

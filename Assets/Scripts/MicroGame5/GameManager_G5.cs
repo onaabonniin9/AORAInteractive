d64f95879e64e0261c36d5d92d86fadd858accba
agarciaalvarez8-gif <agarciaalvarez8@uoc.edu>
2026-04-17 18:55:59 +0200

    últimos cambios

--- Assets/Scripts/MicroGame5/GameManager_G5.cs
+++ Assets/Scripts/MicroGame5/GameManager_G5.cs
@@ -138,26 +138,19 @@ public class GameManager_G5 : MonoBehaviour
 
     void EndGame()
     {
-        bool win = score >= 10;
+        Debug.Log("FIN DEL JUEGO");
 
-        if (gameOverText != null)
+        if (score >= 10)
         {
-            gameOverText.text = win
-                ? "¡HAS GANADO!\nPuntuación: " + score
-                : "HAS PERDIDO\nPuntuación: " + score;
-
-            gameOverText.gameObject.SetActive(true);
-        }
-
-        Time.timeScale = 0f;
-
-        if (win)
-        {
-            GlobalGameManager.instance.WinLevel(score);
+            gameOverText.text = "¡HAS GANADO!\nPuntuación: " + score;
         }
         else
         {
-            GlobalGameManager.instance.LoseLevel();
+            gameOverText.text = "HAS PERDIDO\nPuntuación: " + score;
         }
+
+        gameOverText.gameObject.SetActive(true);
+
+        Time.timeScale = 0f;
     }
 }
